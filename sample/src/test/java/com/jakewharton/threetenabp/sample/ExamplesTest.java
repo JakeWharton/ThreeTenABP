0a971d9f1022ba4f9e0f6046c1a269187de4d2b9
Jake Wharton <jw@squareup.com>
2015-08-02 17:44:39 -0400

    Correct multiple-initialization bug.

---
+++ sample/src/test/java/com/jakewharton/threetenabp/sample/ExamplesTest.java
@@ -0,0 +1,27 @@
+package com.jakewharton.threetenabp.sample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+import org.threeten.bp.Instant;
+
+import static org.junit.Assert.assertNotNull;
+
+/** This class has two tests to ensure that we can initialize the library multiple times. */
+@Config(constants = BuildConfig.class, sdk = 21)
+@RunWith(RobolectricGradleTestRunner.class)
+public final class ExamplesTest {
+  @Test public void one() {
+    Examples activity = Robolectric.setupActivity(Examples.class);
+    Instant now = activity.now();
+    assertNotNull(now);
+  }
+
+  @Test public void two() {
+    Examples activity = Robolectric.setupActivity(Examples.class);
+    Instant now = activity.now();
+    assertNotNull(now);
+  }
+}
