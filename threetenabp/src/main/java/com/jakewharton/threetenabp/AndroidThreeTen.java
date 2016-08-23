7e4625addf22ecee29d73e6fa66377bf741256e5
Egor Neliuba <eneliuba@gmail.com>
2016-08-23 10:14:43 +0300

    Add a version of the init method that accepts Context as a parameter
    Closes #34

--- threetenabp/src/main/java/com/jakewharton/threetenabp/AndroidThreeTen.java
+++ threetenabp/src/main/java/com/jakewharton/threetenabp/AndroidThreeTen.java
@@ -1,6 +1,7 @@
 package com.jakewharton.threetenabp;
 
 import android.app.Application;
+import android.content.Context;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -12,6 +13,10 @@ public final class AndroidThreeTen {
   private static final AtomicBoolean initialized = new AtomicBoolean();
 
   public static void init(Application application) {
+    init((Context) application);
+  }
+
+  public static void init(Context context) {
     if (initialized.getAndSet(true)) {
       return;
     }
@@ -19,7 +24,7 @@ public final class AndroidThreeTen {
     TzdbZoneRulesProvider provider;
     InputStream is = null;
     try {
-      is = application.getAssets().open("org/threeten/bp/TZDB.dat");
+      is = context.getAssets().open("org/threeten/bp/TZDB.dat");
       provider = new TzdbZoneRulesProvider(is);
     } catch (IOException e) {
       throw new IllegalStateException("TZDB.dat missing from assets.", e);
