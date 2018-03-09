faacd66ec1e660785173083f18b05bcbbc64cb3b
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-03-09 15:30:28 +0100

    Update everything to AGP 3.0.1 and alike. (#75)

--- sample/src/test/java/com/jakewharton/threetenabp/sample/ExamplesTest.java
+++ sample/src/test/java/com/jakewharton/threetenabp/sample/ExamplesTest.java
@@ -4,13 +4,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
 import org.threeten.bp.Instant;
 
 import static org.junit.Assert.assertNotNull;
 
 /** This class has two tests to ensure that we can initialize the library multiple times. */
-@Config(constants = BuildConfig.class, sdk = 21)
 @RunWith(RobolectricTestRunner.class)
 public final class ExamplesTest {
   @Test public void one() {
