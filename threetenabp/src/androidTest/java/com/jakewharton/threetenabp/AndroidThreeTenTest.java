78a5fa3367cc81fbb536c4c30b974ea9b764c106
Mitchell Tilbrook <mtilbrook@moneytree.jp>
2019-03-04 08:59:22 +1100

    Allow for custom TZDB

--- threetenabp/src/androidTest/java/com/jakewharton/threetenabp/AndroidThreeTenTest.java
+++ threetenabp/src/androidTest/java/com/jakewharton/threetenabp/AndroidThreeTenTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.threeten.bp.zone.ZoneRulesProvider;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.fail;
 
 public final class AndroidThreeTenTest {
   private final Context context =
@@ -14,5 +15,17 @@ public final class AndroidThreeTenTest {
   @Test public void litmus() {
     AndroidThreeTen.init(context);
     assertThat(ZoneRulesProvider.getAvailableZoneIds()).isNotEmpty();
+  }
+
+  @Test public void customPath() {
+    AndroidThreeTen.init(context, "does/not/exist.dat");
+    try {
+      // This will trigger class loading and parsing of the supplied file.
+      ZoneRulesProvider.getAvailableZoneIds();
+      fail();
+    } catch (ExceptionInInitializerError e) {
+      assertThat(e).hasCauseThat().isInstanceOf(IllegalStateException.class);
+      assertThat(e).hasCauseThat().hasMessageThat().isEqualTo("does/not/exist.dat missing from assets");
+    }
   }
 }
