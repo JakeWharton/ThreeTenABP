4e147ee6c887c92812c2d120c6a9cc623015191f
Drew Hamilton <drew.hamilton.0@gmail.com>
2019-03-15 16:40:53 +0100

    Use AndroidDateTimeFormatter in Examples

--- sample/src/main/java/com/jakewharton/threetenabp/sample/Examples.java
+++ sample/src/main/java/com/jakewharton/threetenabp/sample/Examples.java
@@ -3,7 +3,14 @@ package com.jakewharton.threetenabp.sample;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import com.jakewharton.threetenabp.format.AndroidDateTimeFormatter;
+
 import org.threeten.bp.Instant;
+import org.threeten.bp.ZoneId;
+import org.threeten.bp.ZonedDateTime;
+import org.threeten.bp.format.DateTimeFormatter;
+import org.threeten.bp.format.FormatStyle;
 
 import static android.util.TypedValue.COMPLEX_UNIT_SP;
 import static android.view.Gravity.CENTER;
@@ -12,14 +19,20 @@ public final class Examples extends Activity {
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
+    DateTimeFormatter formatter = AndroidDateTimeFormatter.ofLocalizedTime(this);
+
     TextView tv = new TextView(this);
     tv.setGravity(CENTER);
     tv.setTextSize(COMPLEX_UNIT_SP, 20);
-    tv.setText("NOW: " + now());
+    tv.setText("NOW: " + formatter.format(hereAndNow()));
     setContentView(tv);
   }
 
   public Instant now() {
     return Instant.now();
+  }
+
+  public ZonedDateTime hereAndNow() {
+    return ZonedDateTime.ofInstant(now(), ZoneId.systemDefault());
   }
 }
